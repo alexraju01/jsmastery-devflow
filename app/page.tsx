eeabc7015de32fb364669275c66b7e6d6d0b77de
CodeExcalibar <Alex.raju1010@gmail.com>
2025-01-22 19:28:44 +0000

    Setup fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 const Home = () => (
-  <h1 className="text-3xl  font-black text-primary-500">
-    Welcome to the world of next.js
-  </h1>
+  <>
+    <h1 className="text-3xl  font-black text-primary-500">Welcome to the world of next.js</h1>
+    <h1 className="font-space-grotesk text-3xl font-black text-primary-500">
+      Welcome to the world of next.js
+    </h1>
+  </>
 );
 
 export default Home;
