956b3f1ac3f706f9c69ac7c2725f72330962f3f7
CodeExcalibar <Alex.raju1010@gmail.com>
2025-03-08 20:00:04 +0000

    Implement LeftSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,20 @@
 import { ReactNode } from "react";
 
-import Navbar from "@/components/navigation/navbar";
+import LeftSidebar from "@/components/navigation/navbar/LeftSidebar";
+import Navbar from "@/components/navigation/navbar/Navbar";
+// import { Menu } from "lucide-react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
+    <main className="background-light850_dark100 relative">
       <Navbar />
-
-      {children}
-    </div>
+      <div className="flex">
+        <LeftSidebar />
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+      </div>
+    </main>
   );
 };
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,10 +5,7 @@ const Home = async () => {
   console.log(session);
   return (
     <>
-      <h1 className="text-3xl  font-black text-primary-500">Welcome to the world of next.js</h1>
-      <h1 className="font-space-grotesk text-3xl font-black text-primary-500">
-        Welcome to the world of next.js
-      </h1>
+      <h1 className="text-3xl font-black text-primary-500">Welcome to the world of next.js</h1>
     </>
   );
 };
