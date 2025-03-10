bb8ec2d0419519557fd8adb6b65fb35563bffa51
CodeExcalibar <Alex.raju1010@gmail.com>
2025-03-10 14:49:59 +0000

    corrected the styling on question card

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import React from "react";
 
 import ROUTES from "@/constants/routes";
 import { getTimeStamp } from "@/lib/utils";
@@ -9,10 +10,10 @@ import Metric from "../Metric";
 interface Props {
   question: Question;
 }
+
 const QuestionCard = ({
   question: { _id, title, tags, author, createdAt, upvotes, answers, views },
 }: Props) => {
-  console.log(createdAt);
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
@@ -20,6 +21,7 @@ const QuestionCard = ({
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
             {getTimeStamp(createdAt)}
           </span>
+
           <Link href={ROUTES.QUESTION(_id)}>
             <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
               {title}
@@ -33,7 +35,8 @@ const QuestionCard = ({
           <TagCard key={tag._id} _id={tag._id} name={tag.name} compact />
         ))}
       </div>
-      <div className="flex-between mt-6 w-full">
+
+      <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
           imgUrl={author.image}
           alt={author.name}
@@ -43,6 +46,7 @@ const QuestionCard = ({
           textStyles="body-medium text-dark400_light700"
           isAuthor
         />
+
         <div className="flex items-center gap-3 max-sm:flex-wrap max-sm:justify-start">
           <Metric
             imgUrl="/icons/like.svg"
